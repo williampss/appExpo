74f88cdf309f2a9f0a8c24c91e4dca721d628b14
williampss <149440991+williampss@users.noreply.github.com>
2024-12-06 11:53:55 -0300

    Update index.tsx
    modificado para o código LoginAnimation

--- App-Comida/src/app/index.tsx
+++ App-Comida/src/app/index.tsx
@@ -1,59 +1,106 @@
-import { View, ScrollView } from "react-native";
-import Constants from 'expo-constants'
+import React from 'react';
+import { View, Text, Stylesheet } from "react-native";
 
-import { Header } from "../components/header";
-import { Banner } from "../components/banner";
-import { Search } from "../components/search";
-import { Section } from "../components/section";
-import { TrendingFoods } from "../components/trending";
-import Restaurants from "../components/restaurants";
-import { RestaurantVerticalList } from "../components/restaurantsList"
+import * as Animatable from 'react-native-animatable'
 
-const statusBarHeight = Constants.statusBarHeight;
 
-export default function Index() {
+export default function Signin() {
   return (
-    <ScrollView
-      style={{ flex: 1 }}
-      className="bg-slate-200"
-      showsVerticalScrollIndicator={false}
-    >
 
-      <View className="w-full px-4" style={{ marginTop: statusBarHeight + 8 }}>
-        <Header />
+    <View style={styles.conatainer}>
+      <Animatable.View animation="fadeInLeft" delay={500} style={styles.containerHeader}>
+          <Text style={styles.message}> Bem-Vindo </Text>
+          
+      </Animatable.View>
+      <Animatable.View animation="fadeInUp" style={styles.containerForm}>
+          <Text style={styles.title}> Email </Text>
+          <TextInput
+            placeHolder="Digite um email..."
+            style={styles.input}
+          />
+          <Text style={styles.title}> Senha </Text>
+          <TextInput
+            placeHolder="Digite sua senha"
+            style={styles.input}
+          />
 
-        <Banner />
+          <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Acessar</Text>
+          </TouchableOpacity>
 
-        <Search />
-      </View>
-
-      <Section
-        name="Comidas em alta"
-        label="Veja mais"
-        size="text-2xl"
-        action={() => console.log("CLICOU NO VEJA MAIS")}
-      />
-
-      <TrendingFoods/>
-
-      <Section
-        name="Famosos no DevFoods"
-        label="Veja todos"
-        size="text-xl"
-        action={() => console.log("CLICOU NO FAMOSOS")}
-      />
-      
-      <Restaurants/>
-
-      <Section
-        name="Restaurantes"
-        label="Veja todos"
-        size="text-xl"
-        action={() => console.log("CLICOU NO RESTAURANTES")}
-      />
-
-      <RestaurantVerticalList/>
-
-    </ScrollView>
+         <TouchableOpacity style={styles.buttonRegister}>
+              <Text style={styles.registerText}>Não possui uma conta? Cadastre-se</Text>
+          </TouchableOpacity>
+        
+      </Animatable.View>
+    </View>
+  
+    
   );
 }
+
+const styles = StylesSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: '#38a69d' 
+  },
+  
+  containerHeader:{
+    marginTop: '14%',
+    marginBottom: '8%',
+    paddingStart: '5%'
+  },
+  
+   massage:{
+     fontSize: 28,
+     fontWeight: 'bold',
+     color: "#FFF"   
+  },
+  
+  containerForm:{
+    flex: 1,
+    backgroundColor: '#FFF',
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingStart: '5%',
+    paddingEnd: '5%'
+  },
+  
+  title:{
+    fontSize: 20,
+    marginTop: 28,
+  },
+  
+  input:{
+    borderBottonWidth: 1,
+    height: 40,
+    marginBottom: 8,
+    fontSize: 16,
+  },
+
+  button:{
+    backgroundColor: '#38a69d',
+    width: '100%',
+    borderRadius: 4,
+    paddingVertical: 8,
+    marginTop: 14,
+    justfyContent: 'center',
+    alingItems: 'center'
+  },
+  
+   buttonText:{
+    color: '#FFF',
+    fontWeigth: 'bold'  
+  },
+  
+   buttonRegister:{
+    marginTop: 14,
+    alingSelf: 'center'   
+  },
+  
+   registerText:{
+    color: '#a1a1a1'   
+  },
+                                  
+
+                                  
